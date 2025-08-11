29a3e2e23fe8003bdf55f46546e0ef9cba413e84
JamesCalingo <james.calingo.web@gmail.com>
2025-08-11 00:03:49 -0400

    testing mobile margin

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -15,11 +15,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/index.js"
     ],
-    "/[post]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/[post].js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
