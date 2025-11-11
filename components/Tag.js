b65c7829b0042afa9743d694ddad38d291ec6530
JamesCalingo <james.calingo.web@gmail.com>
2025-11-11 10:37:47 -0500

    adding hockey east to fleet

--- components/Tag.js
+++ components/Tag.js
@@ -1,8 +1,10 @@
+import Link from "next/link"
+
 function Tag(props) {
     const { tag, index, last } = props
     return (
 
-        <span className="tag"> {tag} {index !== last && "|"}</span>
+        <span className="tag"> <Link href={`/tagged/${tag}`}> {tag}</Link> {index !== last && "|"}</span>
 
     )
 }
