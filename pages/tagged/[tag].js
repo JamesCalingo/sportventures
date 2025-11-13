73632c675db1b6d0e13cc6dec563a7b7c95c8551
JamesCalingo <james.calingo.web@gmail.com>
2025-11-13 11:01:46 -0500

    hiding tag page for now

--- pages/tagged/[tag].js
+++ pages/tagged/[tag].js
@@ -1,44 +1,44 @@
-import { posts } from "../../posts";
-import PostList from "../../components/PostList";
-import Layout from "../../components/Layout";
-import {tags} from "../../utils/utils";
-console.log(tags)
+// import { posts } from "../../posts";
+// import PostList from "../../components/PostList";
+// import Layout from "../../components/Layout";
+// import {tags} from "../../utils/utils";
+// console.log(tags)
 
-const Tagged = ({ posts }, tag) => {
+// const Tagged = ({ posts }, tag) => {
 
-    const published = posts.filter((post) => post.frontmatter.published === true);
-    const tagged = published.filter(post => post.frontmatter.tags && post.frontmatter.tags.includes(tag)).reverse()
+//     const published = posts.filter((post) => post.frontmatter.published === true);
+//     const tagged = published.filter(post => post.frontmatter.tags && post.frontmatter.tags.includes(tag)).reverse()
 
 
-    return (
-        <Layout pageTitle={`${tag} - Sportventures`}>
+//     return (
+//         <Layout pageTitle={`${tag} - Sportventures`}>
 
-            <div className="section-header">
-                <span>Posts tagged "{tag}"</span>
-            </div>
-            <PostList posts={tagged} />
+//             <div className="section-header">
+//                 <span>Posts tagged "{tag}"</span>
+//             </div>
+//             <PostList posts={tagged} />
 
-        </Layout>
-    );
-};
+//         </Layout>
+//     );
+// };
 
-export async function getStaticProps() {
-    const tagProps = tags;
+// export async function getStaticProps() {
+//     const tagProps = tags;
 
-    return {
-        props: {
-            tags: tagProps,
-        },
-    };
-}
+//     return {
+//         props: {
+//             tags: tagProps,
+//         },
+//     };
+// }
 
-export async function getStaticPaths() {
-  const paths = tags.map((slug) => `/tagged/${slug}`);
+// export async function getStaticPaths() {
+//   const paths = tags.map((slug) => `/tagged/${slug}`);
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-export default Tagged;
+// export default Tagged;
